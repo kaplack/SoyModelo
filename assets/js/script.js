5fe63a3c0e640b91ae3731ebbd0a9a76a8f2e98e
Alan Burga <ahbb21@hotmail.com>
2022-01-30 21:42:06 -0500

    Soy Modelo v1.7 | Nuevo estilo terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
-const ham = document.querySelector('.menu-mobile');
-const mobileMenu= document.querySelector('.menu-bar');
-const close = document.querySelector('.menu-mobile__close');
+const ham = document.querySelector(".menu-mobile");
+const mobileMenu = document.querySelector(".menu-bar");
+const close = document.querySelector(".menu-mobile__close");
 
-
-ham.addEventListener('click', ()=>{
-    mobileMenu.classList.toggle('active');
+ham.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
 });
 
-close.addEventListener('click', ()=>{
-    mobileMenu.classList.toggle('active');
-})
+close.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
+/*
+let nombre = prompt("Cual es tu nombre?");
+let yearOfBirth = parseInt(prompt("Ingresa Año de nacimiento"));
+
+let mensaje = "Mi nombre es " + nombre + " y tengo " + (2022 - yearOfBirth);
+alert(mensaje);
+
+let i = 0;
+
+i = i + 1;
+
+console.log(i);
+
+const r = 0;
+
+r = r + 1;
+
+console.log(r);*/
